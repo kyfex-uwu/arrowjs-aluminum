58a9d55b08b7d4f4e8a9bc7315460613944cfded
kyfex-uwu <kyfexuwu@gmail.com>
2025-10-28 22:04:09 -0600

    making stuff better :3

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import {ref, Ref} from "./utils.js";
+import Router, {PageAttachedRouter} from "./Router.js";
+import createGenerator, {ArrowElementGenerator} from "./ArrowElementGenerator.js";
+
+export {
+    ref, Ref,
+    Router, PageAttachedRouter,
+    createGenerator, ArrowElementGenerator
+}
